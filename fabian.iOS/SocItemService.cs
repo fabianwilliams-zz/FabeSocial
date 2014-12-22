2b2db79921032064129cb886cdcbdb926a01cdaa
Fabian Williams <fabian@adotob.com>
2014-12-21 21:54:26 -0500

    add new proj

---
+++ fabian.iOS/SocItemService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+
+
+namespace fabian.iOS
+{
+	public class SocItemService
+	{
+		public SocItemService ()
+		{
+		}
+	}
+}
+
---
+++ fabian.iOS/SocialItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace fabian.iOS
+{
+	public class SocialItem
+	{
+		public string Id { get; set; }
+		[JsonProperty(PropertyName = "firstname")]
+		public string FirstName { get; set; }
+		[JsonProperty(PropertyName = "lastname")]
+		public string LastName { get; set; }
+		[JsonProperty(PropertyName = "mobilephone")]
+		public string MobilePhone { get; set; }
+		[JsonProperty(PropertyName = "emailaddr")]
+		public string EmailAddr { get; set; }
+		[JsonProperty(PropertyName = "twitterhandle")]
+		public string TwitterHandle { get; set; }
+		[JsonProperty(PropertyName = "igname")]
+		public string IGName { get; set; }
+		[JsonProperty(PropertyName = "fbname")]
+		public string FBName { get; set; }
+	}
+}
